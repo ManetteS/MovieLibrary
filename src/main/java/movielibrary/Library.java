69d2f6b49fcbcc7d0ae87d033199c87590e6a86c
ManetteS <Manette@172.17.3.70>
2017-02-02 22:01:14 +0100

    created main methods in Library : addMovie, retrieveMovie, updateMovie and deleteMovie

--- src/main/java/movielibrary/Library.java
+++ src/main/java/movielibrary/Library.java
@@ -13,15 +13,19 @@ public class Library {
 		this.movies.add(new Movie(title, releaseYear, genre, actors, plot));
 	}
 	
-	public void retrieveMovie() {
-		
+	public Movie retrieveMovie(int id) {
+		return this.movies.get(id);
 	}
 	
-	public void updateMovie() {
-		
+	public void updateMovie(int id, String title, int releaseYear, String genre, String actors, String plot) {
+		this.movies.get(id).setTitle(title);
+		this.movies.get(id).setReleaseYear(releaseYear);
+		this.movies.get(id).setGenre(genre);
+		this.movies.get(id).setActors(actors);
+		this.movies.get(id).setPlot(plot);
 	}
 	
-	public void deleteMovie() {
-		
+	public void deleteMovie(int id) {
+		this.movies.remove(id);
 	}
 }
