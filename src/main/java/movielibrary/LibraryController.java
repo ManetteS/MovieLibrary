6a1f6447474ee1e0d3b7f3b0d50e9433333174dd
ManetteS <Manette@172.17.3.70>
2017-02-04 17:11:37 +0100

    added Library resource to LibraryController

--- src/main/java/movielibrary/LibraryController.java
+++ src/main/java/movielibrary/LibraryController.java
@@ -1,14 +1,21 @@
 package movielibrary;
 
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LibraryController {
 	
+	@Resource
+	private Library library;
+	
 	@RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    public ArrayList<Movie> showLibrary() {
+        return library.display();
     }
 	
 }
